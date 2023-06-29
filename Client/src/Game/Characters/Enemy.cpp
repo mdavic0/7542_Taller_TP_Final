61e260e6b2db785119dea80fe7ada69b5cea83a7
JosEduardo93 <jchavez@fi.uba.ar>
2023-06-29 00:37:29 -0300

    fix change render infecteds

--- Client/src/Game/Characters/Enemy.cpp
+++ Client/src/Game/Characters/Enemy.cpp
@@ -81,7 +81,7 @@ void Enemy::renderAnimation(int speed, SDL_Texture* texture, SDL_Rect camera) {
                                 SIZE_FRAME_ENEMY, SIZE_FRAME_ENEMY};
         rectFinal = {   position.first - camera.x,
                         position.second - camera.y,
-                        SIZE_FRAME_ENEMY, SIZE_FRAME_ENEMY};
+                        SIZE_FRAME, SIZE_FRAME};
     } else if (type == TypeInfected::infected_spear ||
                 type == TypeInfected::infected_venom) {
         rectInit = {   (speedAnimation * (SIZE_FRAME)), 0,
@@ -112,7 +112,7 @@ void Enemy::renderDead(int speed, SDL_Texture* texture, SDL_Rect camera) {
                                 SIZE_FRAME_ENEMY, SIZE_FRAME_ENEMY};
         rectFinal = {   position.first - camera.x,
                         position.second - camera.y,
-                        SIZE_FRAME_ENEMY, SIZE_FRAME_ENEMY};
+                        SIZE_FRAME, SIZE_FRAME};
     } else if (type == TypeInfected::infected_spear ||
                 type == TypeInfected::infected_venom) {
         rectInit = {   (speedAnimation * (SIZE_FRAME)), 0,
